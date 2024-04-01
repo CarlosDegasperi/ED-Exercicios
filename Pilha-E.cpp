2c70a07f36a5a78381b8160e0e219d6b628594f1
Carlos Armindo Degasperi <111812436+CarlosDegasperi@users.noreply.github.com>
2024-04-01 15:41:46 -0300

    Add files via upload

--- Pilha-E.cpp
+++ Pilha-E.cpp
@@ -17,47 +17,50 @@ struct pilha {
     int size;
 };
 /* Definir as funções */
-void inicializar(pilha*);
-void estacionar(pilha*, string, int);
-void estacionar(pilha*, carro);
-carro sair(pilha*);
-void imprime_pilha(pilha*);
-void imprime_tudo(pilha*);
-int qtde_vagas(pilha);
+void INICIALIZAR(pilha*);
+void ESTACIONAR(pilha*, string, int);
+void ESTACIONAR(pilha*, carro);
+carro SAIR(pilha*);
+void IMPRIME_PILHA(pilha*);
+void IMPRIME_TUDO(pilha*);
+int QTDE_VAGAS(pilha);
 
 int main() {
-    pilha estacionamento;
+    setlocale(LC_ALL, "Portuguese");
+    
+	pilha estacionamento;
     /* Inicializar a Pilha */
-	inicializar(&estacionamento);
+	INICIALIZAR(&estacionamento);
     /* Estacionar + 2 Carros */
-    cout << "- Total de VAGAS do Estacionamento: " <<qtde_vagas(estacionamento) << "\n";
-    estacionar(&estacionamento, "ABC-1234", 1999);
-    estacionar(&estacionamento, "DOW-5782", 2004);
+    cout << "- Total de VAGAS do Estacionamento: " <<QTDE_VAGAS(estacionamento) << "\n";
+    ESTACIONAR(&estacionamento, "ABC-1234", 1999);
+    carro carro_entrada =      {"DOW-5782", 2004};
+    ESTACIONAR(&estacionamento, carro_entrada);
 	/* Mostrar como está o Estacionamento */
-	cout << "- Entrou 2 Carros...\n";
-    cout << "- Vagas Disponíveis: " << qtde_vagas(estacionamento) << "\n";
-    imprime_pilha(&estacionamento);
+	cout << "- Entrou  2 Carros...\n";
+    cout << "- Vagas Disponíveis: " << QTDE_VAGAS(estacionamento) << "\n";
+    IMPRIME_PILHA(&estacionamento);
     /* Estacionar + 4 carros */
-    estacionar(&estacionamento, "XYZ-3215", 2022);
-    estacionar(&estacionamento, "WQT-2245", 2020);
-    estacionar(&estacionamento, "GIN-3285", 2017);
-    estacionar(&estacionamento, "HIX-5215", 2008);
+    ESTACIONAR(&estacionamento, "XYZ-3215", 2022);
+    ESTACIONAR(&estacionamento, "WQT-2245", 2020);
+    ESTACIONAR(&estacionamento, "GIN-3285", 2017);
+    ESTACIONAR(&estacionamento, "HIX-5215", 2008);
     /* Mostrar como está o Estacionamento */
     cout << "- Entrou +4 Carros...\n"; 
-	cout << "- Vagas Disponíveis:" << qtde_vagas(estacionamento) << "\n";
-	imprime_pilha(&estacionamento);
+	cout << "- Vagas Disponíveis:" << QTDE_VAGAS(estacionamento) << "\n";
+	IMPRIME_PILHA(&estacionamento);
     /* Sair do Estacionamento Carro HIX-5215 */
 	carro carro_saida = {"HIX-5215", 2008};
-	carro_saida = sair(&estacionamento);
+	carro_saida = SAIR(&estacionamento);
     /* Mostrar como está o Estacionamento */
 	cout << "- Saiu do Estacionamento o CARRO: " << carro_saida.placa << "\n";
-    cout << "- Vagas Disponíveis:" << qtde_vagas(estacionamento) << "\n"; 
-	imprime_pilha(&estacionamento);
+    cout << "- Vagas Disponíveis:" << QTDE_VAGAS(estacionamento) << "\n"; 
+	IMPRIME_PILHA(&estacionamento);
     /* Imprimir TUDO */
-    imprime_tudo(&estacionamento);
+    IMPRIME_TUDO(&estacionamento);
 }
 /* 1) Função inicializar */
-void inicializar(pilha* e) {
+void INICIALIZAR(pilha* e) {
     e->size = 0;
     /* Inicializar com AAA-000 e Ano 0 */
 	for(int i = TOTAL_VAGAS-1; i >= 0; i--) {
@@ -65,21 +68,21 @@ void inicializar(pilha* e) {
     }
 }
 /* 2) Funçao estacionar (recebe placa no segundo e ano no terceiro argumento) */
-void estacionar(pilha* e, string placa, int ano) {
+void ESTACIONAR(pilha* e, string placa, int ano) {
     e->data[e->size++] = {placa, ano};
 }
 /* 3) Funçao estacionar (recebe o carro no segundo argumento) */
-void estacionar(pilha* e, carro carro_entrada) {
+void ESTACIONAR(pilha* e, carro carro_entrada) {
     e->data[e->size++] = carro_entrada;
 }
 /* 4) Funcao Sair (Atribuir placa "XXX-000" e ano 0 */
-carro sair(pilha* e) {
+carro SAIR(pilha* e) {
     carro carro_saida = e->data[--e->size];
     e->data[e->size] = {"XXX-0000", 0};
     return carro_saida;
 }
 /* 5) Função Imprime_pilha  */
-void imprime_pilha(pilha* e) {
+void IMPRIME_PILHA(pilha* e) {
     cout << "- Carros Estacionados (PILHA DE CARROS):\n";
     cout << "========================================\n";
     for(int i = e->size-1; i >= 0; i--) {
@@ -87,7 +90,7 @@ void imprime_pilha(pilha* e) {
     }
 }
 /* 6) Função Imprime_tudo  */
-void imprime_tudo(pilha* e) {
+void IMPRIME_TUDO(pilha* e) {
     cout << "- MOSTRAR TODA A PILHA:\n";
     cout << "=======================\n";
     for(int i = TOTAL_VAGAS -1; i >= 0; i--) {
@@ -95,6 +98,6 @@ void imprime_tudo(pilha* e) {
     }
 }
 /* 7) Quantidade de Vagas no Estacionamento */
-int qtde_vagas(pilha e) {
+int QTDE_VAGAS(pilha e) {
     return TOTAL_VAGAS - e.size;
 }
